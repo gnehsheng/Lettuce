d8322051dfd7aafdf60041de2f09b21a096abc9f
NG YONG SHENG <gnehsheng@gmail.com>
2022-05-23 12:51:10 +0800

    .

--- client/src/components/welcome/StartForm.js
+++ client/src/components/welcome/StartForm.js
@@ -5,140 +5,145 @@ import { Label, Input } from '../common/FormControls';
 import Button from '../common/Button';
 import Card from '../common/Card';
 
-export default function Forms(props) {
-  const Controls = styled.div`
+
+function CustomForm(props) {
+	const { onSubmit, buttonLabel, header, loading } = props;
+	return (
+		<form onSubmit={onSubmit}>
+			<Card padding='20px'>
+				<Header>{header}</Header>
+				{props.children}
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+					}}
+				>
+					<Button
+						type='submit'
+						style={{ alignSelf: 'center', marginTop: '30px' }}
+						secondary
+						isLoading={loading}
+						disabled={loading}
+					>
+						{buttonLabel}
+					</Button>
+				</div>
+			</Card>
+		</form>
+	);
+}
+
+const StartForm = (props) => {
+	const [hostDisplayName, setHostDisplayName] = useState('');
+	const [videoUrl, setVideoUrl] = useState('');
+	const [joinDisplayName, setJoinDisplayName] = useState('');
+	const [joinUrl, setJoinUrl] = useState('');
+
+	const _onHost = (e) => {
+		e.preventDefault();
+		props.onHost(hostDisplayName, videoUrl);
+	};
+
+	const _onJoin = (e) => {
+		e.preventDefault();
+		props.onJoin(joinDisplayName, joinUrl);
+	};
+
+	return (
+		<React.Fragment>
+			<Col md={3}>
+				<CustomForm
+					buttonLabel='Host'
+					header='Start a party!'
+					onSubmit={_onHost}
+					loading={props.hostLoading}
+				>
+					<Controls>
+						<Label htmlFor='username1'>Display Name</Label>
+						<Input
+							type='text'
+							name='username1'
+							value={hostDisplayName}
+							onChange={(e) => setHostDisplayName(e.target.value)}
+							id='name1'
+							placeholder='John The Host'
+							required
+						/>
+					</Controls>
+					<Controls>
+						<Label htmlFor='url'>Youtube URL</Label>
+						<Input
+							type='text'
+							name='url'
+							id='url'
+							value={videoUrl}
+							onChange={(e) => setVideoUrl(e.target.value)}
+							placeholder='https://www.youtube.com/watch?v=zFhfksjf_mY'
+							required
+						/>
+					</Controls>
+				</CustomForm>
+			</Col>
+			<Col md={2} align='center'>
+				<BigHeader>...Or...</BigHeader>
+			</Col>
+			<Col md={3}>
+				<CustomForm
+					buttonLabel='Join'
+					header='Join existing one!'
+					loading={props.loading}
+					onSubmit={_onJoin}
+				>
+					<Controls>
+						<Label htmlFor='username2'>Display Name</Label>
+						<Input
+							type='text'
+							name='username2'
+							value={joinDisplayName}
+							onChange={(e) => setJoinDisplayName(e.target.value)}
+							id='name2'
+							placeholder='Dursley'
+							required
+						/>
+					</Controls>
+					<Controls>
+						<Label htmlFor='url'>Join URL</Label>
+						<Input
+							type='text'
+							name='url'
+							id='url'
+							value={joinUrl}
+							onChange={(e) => setJoinUrl(e.target.value)}
+							placeholder='redparty.netlify.app/room/asD2123f'
+						/>
+					</Controls>
+				</CustomForm>
+			</Col>
+		</React.Fragment>
+	);
+};
+
+const Controls = styled.div`
 	display: flex;
 	flex: 1;
 	flex-direction: column;
 	margin: 15px 0;
-    `;
+`;
 
-  const Header = styled.h1`
+const Header = styled.h1`
 	margin: 0;
 	padding: 0;
 	font-weight: 500;
 	margin-bottom: 50px;
 	font-size: 2em;
 	text-align: center;
-    `;
+`;
 
-  const BigHeader = styled.h1`
+const BigHeader = styled.h1`
 	font-weight: 300;
 	font-size: 2.8em;
 	margin-top: -50px;
-    `;
+`;
 
-  const [hostDisplayName, setHostDisplayName] = useState('');
-  const [videoUrl, setVideoUrl] = useState('');
-  const [joinDisplayName, setJoinDisplayName] = useState('');
-  const [joinUrl, setJoinUrl] = useState('');
-
-  function CustomForm(props) {
-    const {
-      onSubmit, buttonLabel, header, loading,
-    } = props;
-
-    return (
-      <form onSubmit={onSubmit}>
-        <Card padding="20px">
-          <Header>{header}</Header>
-          {props.children}
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <Button
-              type="submit"
-              style={{ alignSelf: 'center', marginTop: '30px' }}
-              secondary
-              isLoading={loading}
-              disabled={loading}
-            >
-              {buttonLabel}
-            </Button>
-          </div>
-        </Card>
-      </form>
-    );
-  }
-
-  const _onHost = (e) => {
-		e.preventDefault();
-		props.onHost(hostDisplayName, videoUrl);
-	}
-
-	const _onJoin = (e) => {
-		e.preventDefault();
-		props.onJoin(joinDisplayName, joinUrl);
-	}
-
-  return (
-    <>
-      <Col md={3}>
-        <CustomForm
-          buttonLabel="Host"
-          header="Start a party!"
-          onSubmit={_onHost}
-          loading={props.hostLoading}
-        >
-          <Controls>
-            <Label htmlFor="username1">Pick a username</Label>
-            <Input
-              type="text"
-              name="username1"
-              value={hostDisplayName}
-              onChange={(e) => setHostDisplayName(e.target.value)}
-              id="name1"
-              placeholder="Sheng"
-              required
-            />
-          </Controls>
-          <Controls>
-            <Label htmlFor="url">Youtube URL</Label>
-            <Input
-              type="text"
-              name="url"
-              id="url"
-              value={videoUrl}
-              onChange={(e) => setVideoUrl(e.target.value)}
-              placeholder="https://www.youtube.com/"
-              required
-            />
-          </Controls>
-        </CustomForm>
-      </Col>
-      <Col md={2} align="center">
-        <BigHeader>...Or...</BigHeader>
-      </Col>
-      <Col md={3}>
-        <CustomForm
-          buttonLabel="Join"
-          header="Join existing one!"
-          loading={props.loading}
-          onSubmit={_onJoin}
-        >
-          <Controls>
-            <Label htmlFor="username2">Pick a username</Label>
-            <Input
-              type="text"
-              name="username2"
-              value={joinDisplayName}
-              onChange={(e) => setJoinDisplayName(e.target.value)}
-              id="name2"
-              placeholder="Sheng"
-              required
-            />
-          </Controls>
-          <Controls>
-            <Label htmlFor="url">Join URL</Label>
-            <Input
-              type="text"
-              name="url"
-              id="url"
-              value={joinUrl}
-              onChange={(e) => setJoinUrl(e.target.value)}
-              placeholder=""
-            />
-          </Controls>
-        </CustomForm>
-      </Col>
-    </>
-  );
-}
+export default StartForm;
--- client/src/components/welcome/Welcome.js
+++ client/src/components/welcome/Welcome.js
@@ -11,111 +11,114 @@ import { colors } from '../common/colors';
 import { createConnection } from '../../utils/socket';
 import { getVideoId } from '../../utils/helper';
 
-export default function Welcome(props) {
-  const IntroMessage = styled.h1`
+function Welcome(props) {
+	// const [canRedirectToRoom, setRedirect] = useState(false);
+	let formEnd = null;
+	const [hostLoading, setHostLoading] = useState(false);
+
+	const scrollToForm = () => {
+		if (formEnd) {
+			formEnd.scrollIntoView({ behavior: 'smooth' });
+		}
+	};
+
+	const onHost = async (username, videoUrl) => {
+		// use socket id as room address
+		setHostLoading(true);
+		const videoId = getVideoId(videoUrl);
+		const socket = await createConnection(username, null, videoId);
+		setHostLoading(false);
+
+		props.history.push({
+			pathname: `/room/${socket.id}`, // socket.id === roomid
+			state: { hostId: socket.id, username, videoId },
+			socket,
+		});
+	};
+
+	const onJoin = (username, joinUrl) => {
+		// TODO: Add verification for join url
+		const splitUrl = joinUrl.split('/');
+		const roomId = splitUrl[splitUrl.length - 1];
+		props.history.push({
+			pathname: `/room/${roomId}`,
+			state: { username },
+		});
+	};
+  return (
+		<React.Fragment>
+			<TopBar />
+			<Container fluid style={{ height: '92vh' }}>
+				{/* topbar is 8vh in height, so (100 - 8) = 92 */}
+				<Row style={{ paddingTop: '80px' }} align='center'>
+					<Hidden xs>
+						<Col xs={2}></Col>
+					</Hidden>
+
+					{/* --------- Intro Message -------- */}
+					<Col xs={12} md={4}>
+						<IntroMessage>
+							Host{' '}
+							<span style={{ color: colors.primaryColor }}>
+								Youtube
+							</span>{' '}
+							Watch Party with Friends
+						</IntroMessage>
+						<Button
+							style={styles.heroButton}
+							onClick={scrollToForm}
+							primary
+						>
+							Get Started
+						</Button>
+					</Col>
+
+					<Col xs={12} md={5}>
+						<img src='hero-banner.svg' alt='' />
+					</Col>
+
+					<Hidden xs>
+						<Col xs={2}></Col>
+					</Hidden>
+				</Row>
+				<FeatureBox />
+			</Container>
+
+			<Container fluid>
+				<Row align='center' style={styles.formContainer}>
+					<Col md={2}></Col>
+					<StartForm
+						onHost={onHost}
+						onJoin={onJoin}
+						hostLoading={hostLoading}
+					/>
+					<Col md={2}></Col>
+					<div className='dummy' ref={(el) => (formEnd = el)}></div>
+				</Row>
+			</Container>
+		</React.Fragment>
+	);
+}
+
+const IntroMessage = styled.h1`
 	font-weight: 500;
 	margin: 0;
 	padding: 0;
 	font-size: 2.5em;
-    `;
+`;
 
-  const styles = {
-    formContainer: {
-      backgroundImage: 'linear-gradient(#f9f9f9, #fff)',
-      marginBottom: '40px',
-      zIndex: 10,
-      height: '100vh',
-    },
-    heroButton: {
-      margin: '15px 0',
-      minWidth: '200px',
-      padding: '15px 10px',
-    },
-  };
+const styles = {
+	formContainer: {
+		backgroundImage: 'linear-gradient(#f9f9f9, #fff)',
+		marginBottom: '40px',
+		zIndex: 10,
+		height: '100vh',
+	},
+	heroButton: {
+		margin: '15px 0',
+		minWidth: '200px',
+		padding: '15px 10px',
+	},
+};
 
-  let formEnd = null;
-  const [hostLoading, setHostLoading] = useState(false);
-
-  function scrollToForm() {
-    if (formEnd) {
-      formEnd.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-
-  async function onHost(username, videoUrl) {
-    setHostLoading(true);
-    const videoId = getVideoId(videoUrl);
-    const socket = await createConnection(username, null, videoId);
-    setHostLoading(false);
-
-    props.history.push({
-      pathname: `/room/${socket.id}`, // socket.id === roomid
-      state: { hostId: socket.id, username, videoId },
-      socket,
-    });
-  }
-
-  function onJoin(username, joinUrl) {
-    const splitUrl = joinUrl.split('/');
-    const roomId = splitUrl[splitUrl.length - 1];
-    props.history.push({
-      pathname: `/room/${roomId}`,
-      state: { username },
-    });
-  }
-
-  return (
-    <>
-      <TopBar />
-      <Container fluid style={{ height: '92vh' }}>
-        <Row style={{ paddingTop: '80px' }} align="center">
-          <Hidden xs>
-            <Col xs={2} />
-          </Hidden>
-
-          {/* --------- Intro Message -------- */}
-          <Col xs={12} md={4}>
-            <IntroMessage>
-              Watch
-              {' '}
-              <span style={{ color: colors.primaryColor }}>
-                Youtube
-              </span>
-              {' '}
-               with Friends
-            </IntroMessage>
-            <Button
-              style={styles.heroButton}
-              onClick={scrollToForm}
-              primary
-            >
-              Get Started
-            </Button>
-          </Col>
-
-          <Col xs={12} md={5}>
-            <img src="hero-banner.svg" alt="" />
-          </Col>
-
-          <Hidden xs>
-            <Col xs={2} />
-          </Hidden>
-        </Row>
-        <FeatureBox />
-      </Container>
-
-      <Container fluid>
-        <Row align="center" style={styles.formContainer}>
-          <Col md={2} />
-          <StartForm
-            onHost={onHost}
-            onJoin={onJoin}
-            hostLoading={hostLoading}
-          />
-          <Col md={2} />
-          <div className="dummy" ref={(el) => (formEnd = el)} />
-        </Row>
-      </Container>
-    </>
-  );
-}
+export default Welcome;
